8197342deccb90d0dff891bb45b776fa00964d64
xZero707 <xzero@elite7hackers.net>
2018-05-31 02:10:02 +0200

    Update example

--- example.php
+++ example.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace NorthernLights\IPSConnectApi\Example;
 
 use NorthernLights\IPSConnectApi\Action\Login;
+use NorthernLights\IPSConnectApi\Action\PasswordSalt;
 use NorthernLights\IPSConnectApi\Client;
 use NorthernLights\IPSConnectApi\Provider\ConfigProvider;
 use NorthernLights\IPSConnectApi\Provider\ConfigProviderInterface;
@@ -17,10 +18,14 @@ $config = new ConfigProvider([
 
 // Initialize api client and pass ConfigProvider interface instance
 $client = new Client($config);
+$response = $client->do(new PasswordSalt());
 
 // Initialize login action
 $loginAction = new Login();
-$loginAction->setUserId(12);
+$loginAction
+    ->setUserId(12)
+    ->setDisplayName('test_user')
+    ->setPassword('12345678', $response->getActionResult()->salt);
 
 // Perform request with Login action instance
 $response = $client->do($loginAction);
